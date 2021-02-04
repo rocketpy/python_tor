a4ec5f9e941aee02e9cb0cfac10d2fa0a651173e
rocketpy <dimitr.ushakov@gmail.com>
2021-02-04 21:28:33 +0200

    Update torpy.py

--- torpy.py
+++ torpy.py
@@ -6,7 +6,7 @@
 #  pip install torpy
 #  for using TorHttpAdapter with requests library you need install extras: pip3 install torpy[requests]
 
-#  Features
+#  Features:
 """
 No Stem or official Tor client required
 Support v2 hidden services (v2 specification)
@@ -15,6 +15,9 @@ Provide simple TorHttpAdapter for requests library
 Provide simple urllib tor_opener for making requests without any dependencies
 Provide simple Socks5 proxy
 """
+
+#  Console examples check on Github page PyPi page !!!
+
 
 #  Usage examples:
 
@@ -31,3 +34,20 @@ with TorClient() as tor:
             # Now we can communicate with host
             stream.send(b'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % hostname.encode())
             recv = stream.recv(1024)
+
+            
+#  TorHttpAdapter is a convenient Tor adapter for the requests library.
+#  Requests Adapters:  https://2.python-requests.org/en/master/user/advanced/#transport-adapters
+#  The following example shows the usage of TorHttpAdapter for multi-threaded HTTP requests:          
+
+from multiprocessing.pool import ThreadPool
+from torpy.http.requests import tor_requests_session
+
+
+with tor_requests_session() as s:  # returns requests.Session() object
+    links = ['http://nzxj65x32vh2fkhk.onion', 'http://facebookcorewwwi.onion'] * 2
+
+    with ThreadPool(3) as pool:
+        pool.map(s.get, links)   
+    
+    
