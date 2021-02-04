debd911720c090fdb34ff637d02bcc6397f2608d
rocketpy <dimitr.ushakov@gmail.com>
2021-02-04 21:47:28 +0200

    Update requesocks_tor.py

--- requesocks_tor.py
+++ requesocks_tor.py
@@ -1,1 +1,29 @@
+#  Github:  https://github.com/dvska/requesocks
 
+#  pip install requesocks 
+
+
+import requests
+import requesocks
+
+
+requests = requesocks.session()
+requests.proxies = {"http": "socks5://127.0.0.1:8118",
+                   "https": "socks5://127.0.0.1:8118"}
+
+
+"""
+import requests
+
+
+proxy = { 'http':'127.0.0.1:8118','https':'127.0.0.1:8118' }
+r = requests.get('https://www.whatismyip.com/', proxies=proxy)
+#r = requests.get('http://www.whatsmyip.org/')
+print(r)
+"""
+
+#  customization privoxy
+"""
+forward-socks4a / localhost:9050 .  # try 9150
+forward-socks5 / localhost:9050 .   # try 9150
+"""
