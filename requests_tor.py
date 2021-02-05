9348d2eda2fe2b7c9353644f2b13a71f40c5b038
rocketpy <dimitr.ushakov@gmail.com>
2021-02-05 21:14:10 +0200

    Update requests_tor.py

--- requests_tor.py
+++ requests_tor.py
@@ -1,1 +1,10 @@
+import requests
 
+
+#  Tor browser listens on port 9150
+proxies = {'http': 'socks5://localhost:9150',
+           'https': 'socks5://localhost:9150'}
+
+url = 'http://httpbin.org/ip'
+
+print(requests.get(url, proxies=proxies).text)
