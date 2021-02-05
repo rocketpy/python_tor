38064589e9a8933ed08ddbb2461713595a27d011
rocketpy <dimitr.ushakov@gmail.com>
2021-02-05 21:29:18 +0200

    Update requests_tor.py

--- requests_tor.py
+++ requests_tor.py
@@ -1,10 +1,21 @@
+#  pip install -U requests[socks]
 import requests
 
 
 #  Tor browser listens on port 9150
-proxies = {'http': 'socks5://localhost:9150',
-           'https': 'socks5://localhost:9150'}
+proxies = {'http': 'socks5://localhost:9150',  # or 9050
+           'https': 'socks5://localhost:9150'}  # or 9050
 
 url = 'http://httpbin.org/ip'
 
 print(requests.get(url, proxies=proxies).text)
+
+
+"""
+resp = requests.get('http://...', 
+                     proxies=dict(http='socks5://user:pass@host:port',
+                     https='socks5://user:pass@host:port'))                      
+"""
+
+# sometimes need change  socks5://proxyhost:1234 to socks5h://proxyhost:1234  # add  h
+ 
