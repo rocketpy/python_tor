f67461140ef4befc4fa494379dff9265c4ab9307
rocketpy <dimitr.ushakov@gmail.com>
2021-02-07 21:44:45 +0200

    Update pySocks.py

--- pySocks.py
+++ pySocks.py
@@ -4,6 +4,7 @@
 #  PyPi:  https://pypi.org/project/PySocks/
 
 # pip install PySocks
+# pip install -U requests[socks] 
 
 #  Usage
 import socks
@@ -34,3 +35,18 @@ socket.socket = socks.socksocket
 
 urllib2.urlopen("http://www.somesite.com/") # All requests will pass through the SOCKS proxy
  
+ 
+"""
+ # SOCKS5 proxy for HTTP/HTTPS proxiesDict = { 'http' : "socks5://1.2.3.4:1080", 'https' : "socks5://1.2.3.4:1080" }
+ # SOCKS4 proxy for HTTP/HTTPS proxiesDict = { 'http' : "socks4://1.2.3.4:1080", 'https' : "socks4://1.2.3.4:1080" }
+ # HTTP proxy for HTTP/HTTPS proxiesDict = { 'http' : "1.2.3.4:1080", 'https' : "1.2.3.4:1080" } 
+"""
+
+"""
+import urllib2
+import socket
+import socks
+
+socks.set_default_proxy(socks.SOCKS5, "myprivateproxy.net",port=9050)
+socket.socket = socks.socksocket res=urllib2.urlopen(url).read() 
+"""
