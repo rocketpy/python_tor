e49d595c0673d95983bf23b5f48c64800710f4f5
rocketpy <dimitr.ushakov@gmail.com>
2021-02-11 16:37:28 +0200

    Update Tor_Chrome_browser_Selenium.py

--- Tor_Chrome_browser_Selenium.py
+++ Tor_Chrome_browser_Selenium.py
@@ -1,1 +1,13 @@
+import os
+from selenium import webdriver
 
+
+# To use Tor's SOCKS proxy server with chrome, include the socks protocol in the scheme with the --proxy-server option
+# PROXY = "socks5://127.0.0.1:9150" # IP:PORT or HOST:PORT
+
+torexe = os.popen(r'C:\Users\...\Tor\tor.exe')
+PROXY = "socks5://localhost:9150" # IP:PORT or HOST:PORT
+options = webdriver.ChromeOptions()
+options.add_argument('--proxy-server=%s' % PROXY)
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'C:\Utility\BrowserDrivers\chromedriver.exe')
+driver.get("http://check.torproject.org")
