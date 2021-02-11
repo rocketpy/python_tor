33746282fe198f90bf58ef8d7f53ae9d7fde89e3
rocketpy <dimitr.ushakov@gmail.com>
2021-02-11 16:54:23 +0200

    Update Tor_Firefox_Selenium.py

--- Tor_Firefox_Selenium.py
+++ Tor_Firefox_Selenium.py
@@ -1,1 +1,15 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+
+
+torexe = os.popen(r'C:\Users\...\Tor\tor.exe')
+profile = FirefoxProfile(r'C:\Users\...\TorBrowser\Data\Browser\profile.default')
+profile.set_preference('network.proxy.type', 1)
+profile.set_preference('network.proxy.socks', '127.0.0.1')
+profile.set_preference('network.proxy.socks_port', 9150)
+profile.set_preference("network.proxy.socks_remote_dns", False)
+profile.update_preferences()
+driver = webdriver.Firefox(firefox_profile= profile, executable_path=r'C:\Utility\BrowserDrivers\geckodriver.exe')
+driver.get("http://check.torproject.org")
 
