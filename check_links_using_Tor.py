5ec98dcc5cf31739f827c9cefacb3ee05f772588
rocketpy <dimitr.ushakov@gmail.com>
2021-02-13 21:40:13 +0200

    Update check_links_using_Tor.py

--- check_links_using_Tor.py
+++ check_links_using_Tor.py
@@ -1,1 +1,29 @@
+import os
+import json
+import time
+import requests
+import subprocess
 
+#  Checking a Tor onion links active status !
+
+
+class TorActivation:
+    def __ini__(self):
+        pass
+
+    def StartTor(self):
+        subprocess.Popen([f"{os.getcwd()}\\Tor\\tor.exe"])
+        time.sleep(10)
+        print("Tor Proxy is started !")
+        print("Start of checking  !")
+
+    def proxy_build(self):
+        try:
+            self.proxies = {
+            'http': 'socks5h://127.0.0.1:9050',
+            'https': 'socks5h://127.0.0.1:9050'
+            }
+            print("proxy setup is succefully done")
+    
+        except:
+            print("Tor is not activated please run the program again!!!")
