2e37b46dc044e846d8fc961bead8da65de100b7f
rocketpy <dimitr.ushakov@gmail.com>
2021-02-18 23:14:31 +0200

    Update stem_requests_tor.py

--- stem_requests_tor.py
+++ stem_requests_tor.py
@@ -1,1 +1,9 @@
+import socks
+import socket
+import requests
 
+
+socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9150)
+socket.socket = socks.socksocket
+
+print(requests.get('http://icanhazip.com')).content
