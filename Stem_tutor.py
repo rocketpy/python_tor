0871b346a4a796b16fafcf2fb99845123609c60e
rocketpy <dimitr.ushakov@gmail.com>
2021-02-22 23:05:04 +0200

    Update Stem_tutor.py

--- Stem_tutor.py
+++ Stem_tutor.py
@@ -1,1 +1,20 @@
+#  Stem is a Python controller library that allows applications to interact with Tor (https://www.torproject.org/).
+
+#  PyPi: https://pypi.org/project/stem/
+
+#  Docs: https://stem.torproject.org/
+#  Tutorial: https://stem.torproject.org/tutorials.html
+
+# pip install stem
+
+from stem.control import Controller
+
+
+with Controller.from_port(port = 9051) as controller:
+    controller.authenticate()  # provide the password here if you set one
+
+    bytes_read = controller.get_info("traffic/read")
+    bytes_written = controller.get_info("traffic/written")
+
+    print("My Tor relay has read %s bytes and written %s." % (bytes_read, bytes_written))
 
