aef31a12d5cef0e40970abfb3a92f41325068674
rocketpy <dimitr.ushakov@gmail.com>
2021-09-18 20:38:28 +0300

    Update tor_browser_tutor.py

--- Tor_browser/tor_browser_tutor.py
+++ Tor_browser/tor_browser_tutor.py
@@ -1,1 +1,10 @@
+# Tor Browser automation with Selenium
+
+# PyPi: https://pypi.org/project/tbselenium/
+# Github: https://github.com/webfp/tor-browser-selenium 
+
+# pip install tbselenium
+
+# Install geckodriver from the geckodriver releases page. Make sure you install version v0.23.0 version or newer;
+# older versions may not be compatible with the current Tor Browser series.
 
