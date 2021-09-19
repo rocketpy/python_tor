67f7aba0ece7d84a7b46e311bb5f7c25ffe32503
rocketpy <dimitr.ushakov@gmail.com>
2021-09-19 17:59:43 +0300

    Update tor_browser_tutor.py

--- Tor_browser/tor_browser_tutor.py
+++ Tor_browser/tor_browser_tutor.py
@@ -8,3 +8,13 @@
 # Install geckodriver from the geckodriver releases page. Make sure you install version v0.23.0 version or newer;
 # older versions may not be compatible with the current Tor Browser series.
 
+
+# Basic usage:
+# Using with system tor
+
+# tor needs to be installed (apt install tor) and running on port 9050.
+from tbselenium.tbdriver import TorBrowserDriver
+
+with TorBrowserDriver("/path/to/TorBrowserBundle/") as driver:
+    driver.get('https://check.torproject.org')
+
